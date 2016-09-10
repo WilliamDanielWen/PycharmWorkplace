226ccc7ec2c9a67af69365e16f6ddb60167c44c8
WilliamDanielWen <wenyukm@msn.om>
2016-09-10 14:43:50 -0400

    2

---
+++ CS6200_IR_Assignments/HW6_Feature_Extraction/RandomForest.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import Utilities
+from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
+
+# read the data that we need from the original file
+trainSet = pd.read_csv('runtime_cache/train_split1.csv')
+testSet = pd.read_csv('runtime_cache/test_split1.csv')
+
+trainSetFeatures = trainSet.drop(['query_id', 'doc_id', 'label',"query_length"],axis=1).values  # id and relevance is not features to use
+trainSetLabels = trainSet['label'].values
+
+testSetFeatures = testSet.drop(['query_id', 'doc_id', 'label',"query_length"], axis=1).values
+testSetLabels = testSet['label'].values
+
+
+print "\nBegin training..."
+#train the model
+random_forest_regressor = RandomForestRegressor(n_estimators=15, max_depth=8, random_state=10)
+bagging_regressor = BaggingRegressor(random_forest_regressor, n_estimators=50, max_samples=0.1, random_state=25)
+bagging_regressor.fit(trainSetFeatures, trainSetLabels)
+
+print "\nBegin prediction..."
+#make the prediction on the test set
+predictedLabels = bagging_regressor.predict(testSetFeatures)
+predicted_train_Labels = bagging_regressor.predict(trainSetFeatures)
+
+
+
+print "RMSE :\t", Utilities.getRMSE(testSetLabels, predictedLabels)
+print "MAE :\t", Utilities.getMAE(testSetLabels, predictedLabels)
+
+result_path = "runtime_cache/test_performance_random_forest_split1.txt"
+Utilities.outputResult(testSet,predictedLabels,result_path)
+
+result_path = "runtime_cache/train_performance_random_forest_split1.txt"
+Utilities.outputResult(trainSet,predicted_train_Labels,result_path)
